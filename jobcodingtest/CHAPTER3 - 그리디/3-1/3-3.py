2c7df37a2bda3fe2dc4afb1db9eb92c339cfc0ff
JinHeok Kim <bustreedev@gmail.com>
2021-11-04 20:33:20 +0900

    파이썬 그리디 알고리즘 3-3 숫자 카드게임

---
+++ jobcodingtest/CHAPTER3 - 그리디/3-1/3-3.py
@@ -0,0 +1,22 @@
+import time
+
+## 숫자 카드 게임
+# 첫째 줄에 N, M K의 자연수가 주어지며, 각자연수는 공백으로 구분한다.
+# 입력으로 주어지는 K는 항상 M보다 작거나 같다.
+
+inputN = 3
+inputM = 3
+
+cardList = [[3, 1, 2], [4, 1, 4], [2, 2, 2]]
+cardList2 = [[7, 3, 1, 8], [3, 3, 3, 4]]
+resultList = []
+
+for i, n in enumerate(cardList2):
+    n.sort()
+    resultList.append(n[0])
+
+resultList.sort()
+resultList
+
+print('정답')
+print(resultList[-1])
