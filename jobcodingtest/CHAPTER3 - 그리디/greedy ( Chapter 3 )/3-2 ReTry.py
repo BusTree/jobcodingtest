cb026e18303076e6b4d4143630677e3c52692b47
JinHyeok <nox6438@naver.com>
2021-11-06 17:31:42 +0900

    파이썬 알고리즘 공부 그리디 큰 수의 법칙 피드백 2

--- jobcodingtest/CHAPTER3 - 그리디/greedy ( Chapter 3 )/3-2 ReTry.py
+++ jobcodingtest/CHAPTER3 - 그리디/greedy ( Chapter 3 )/3-2 ReTry.py
@@ -6,16 +6,27 @@ n = 6 # 배열의 크기
 m = 150 # m번 더해서 최종적으로 가장큰 수를 구한다.
 k = 9 # 하나의 인덱스 반복수
 
+result = 0
+
 array = [2, 4, 5, 4, 6, 7]
 array.sort()
-result = 0
+
 (a, b) = (array[n-1], array[n-2])
 (c, d) = divmod(m, k + 1)
 
-aa = k * c * a
-bb = c * b
-cc = a * d
+first = k * (c + d) * a  # 1번 배열 값
+second = c * b  # 2번 배열 값
+# cc = a * d # 1번배열값 나머지
 
-result = aa + bb + cc
+result = first + second
 print('result')
 print(result)
+
+# 피드백
+# 처음부터 첫뻔째 베열의 개수를 세는것을 사용한다.
+# count = int(m / k + 1 )) * k
+# count += m % ( k + 1 )
+# result = 0
+# result += (count) * first # 가장큰수 더하기
+# result += (m - count) * second # 두번째로 큰 수 더하기
+# print(result)
