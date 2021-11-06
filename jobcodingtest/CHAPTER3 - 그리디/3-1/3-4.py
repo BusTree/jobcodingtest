10e6f3344e034ec7097de6797978e9c5126534a7
JinHyeok <nox6438@naver.com>
2021-11-06 15:10:25 +0900

    파이썬 알고리즘 공부 그리디 303 1이 될 때까지 문제풀이 - 피드백

--- jobcodingtest/CHAPTER3 - 그리디/3-1/3-4.py
+++ jobcodingtest/CHAPTER3 - 그리디/3-1/3-4.py
@@ -1,17 +1,26 @@
 import time
-
+import math
+math.factorial(100000)
 # 1이 될 때까지
+start_time = time.time()
+
 n = 17
 k = 4
 count = 0
 
-while (n > 1):
+while n != 1:
     (a, b) = divmod(n, k)
     if b == 0:
         n = a
     else:
         n -= 1
     count += 1
+end_time = time.time()
 
 print('result')
 print(count)
+print('결과시간')
+print(f"{end_time - start_time:.5f} sec")
+
+# 피드백
+# n < 1 보다 n != 1로 비교하는게 연산속도가 더 빠르다. 숫자연산 < 논리연산
