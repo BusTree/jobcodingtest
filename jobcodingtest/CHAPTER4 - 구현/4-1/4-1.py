6bc54ecc968b616b0c85066525f8ce75caa28895
JinHeok Kim <bustreedev@gmail.com>
2021-11-08 21:06:48 +0900

    4 -1 문제풀이 수정
    4 -1 문제풀이 수정

--- jobcodingtest/CHAPTER4 - 구현/4-1/4-1.py
+++ jobcodingtest/CHAPTER4 - 구현/4-1/4-1.py
@@ -1,24 +1,21 @@
 import random
 
 # 상하좌우
-map_size = random.randrange(1, 101)
-guide_size = random.randrange(1, 101)
-print('map size')
-print(map_size)
+map_size = random.randrange(1, 100)
+guide_size = random.randrange(1, 100)
 move_type = ['R', 'L', 'U', 'D']
-move_guide = ''
+move_guide = []
 
 for i in range(guide_size):
-    move_guide += random.sample(move_type, 1)[0]
-    move_guide += ' '
+    move_guide.append(random.sample(move_type, 1)[0])
 
-move_guide = move_guide.replace(" ", "")
-move_list = list(move_guide)
-print('move')
-print(move_list)
+print('map_size')
+print(map_size)
+print('move_guide')
+print(move_guide)
 
 x, y = 1, 1
-for move in move_list:
+for move in move_guide:
     if move == "R" and y < map_size:
         y += 1
     elif move == "L" and y > 1:
@@ -28,4 +25,5 @@ for move in move_list:
     elif move == "D" and x < map_size:
         x += 1
 
+print('result')
 print(x, y)
