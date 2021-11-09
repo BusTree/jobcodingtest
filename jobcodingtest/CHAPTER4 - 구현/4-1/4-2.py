4717d340343b74a9b0141621bc78ad216289a2cc
JinHeok Kim <bustreedev@gmail.com>
2021-11-09 20:33:08 +0900

    파이썬 그리디 알고리즘 4-2 시각 경우의수

---
+++ jobcodingtest/CHAPTER4 - 구현/4-1/4-2.py
@@ -0,0 +1,15 @@
+# 시각
+n = 14 # user Input
+mm = 60
+ss = 60
+result = 0
+
+for h in range(n+1):
+    for m in range(mm):
+        for s in range(ss):
+            check_time = str(h) + str(m) + str(s)
+            if '3' in str(check_time):
+                result += 1
+
+print('result')
+print(result)
