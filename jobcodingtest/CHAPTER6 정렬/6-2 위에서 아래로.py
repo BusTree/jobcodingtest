e6e02ddd7d103f7366999d859cc225d79d0c54cf
JinHeok Kim <bustreedev@gmail.com>
2021-11-16 21:18:26 +0900

    6-2 위에서 아래로 ( 정렬문제 ) - 개선

--- jobcodingtest/CHAPTER6 정렬/6-2 위에서 아래로.py
+++ jobcodingtest/CHAPTER6 정렬/6-2 위에서 아래로.py
@@ -1,3 +1,14 @@
-input_list = [3, 15, 27, 12]
-input_list.sort(reverse=True)
-print(input_list)
+import random
+
+n = input()
+
+array = []
+# array = [3, 15, 27, 12]
+
+for i in len(n):
+    array.append(random.randrange(1, 501))
+
+array.sort(reverse=True)
+
+for i in array:
+    print(i, end=' ')
