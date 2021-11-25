d5c6a637448bd489ee5afac467453e3b8568ab1a
JinHeok Kim <bustreedev@gmail.com>
2021-11-25 21:45:00 +0900

    8-5 효율적인 화폐구성 ( 완료 )

---
+++ jobcodingtest/CHAPTER8 다이나익 프로그래밍/7-5 효율적인 화폐 구성.py
@@ -0,0 +1,23 @@
+n = 2
+m = 15
+
+moeny_type = [2, 3]
+dp = []
+dp.append(m)
+
+while m > 0:
+    array = []
+    for i in range(n):
+        value = dp[-1] - moeny_type[i]
+        array.append(value)
+    dp.append(min(array))
+    if dp[-1] <= 0:
+        break
+
+if(dp[-1] == 0):
+    print(dp[-2])
+
+else:
+    print(-1)
+
+print(dp)
