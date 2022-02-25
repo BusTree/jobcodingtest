72e568b00ab1b9aa9832de9551abab7531b5135f
JinHeok Kim <bustreedev@gmail.com>
2022-02-25 21:20:17 +0900

    감시 피하기 문제풀이 1차

---
+++ jobcodingtest/프로그래머스 문제풀이/20_감시 피하기.py
@@ -0,0 +1,55 @@
+# 감시피하기
+# n x n 벽
+# 학생 S 장애물 O 선생 T
+
+from itertools import combinations
+input = 5
+
+# 4가지 이동 방향에 대한 리스트
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+world_map = [
+    ['X', 'S', 'X', 'X', 'T'],
+    ['T', 'X', 'S', 'X', 'X'],
+    ['X', 'X', 'X', 'X', 'X'],
+    ['X', 'T', 'X', 'X', 'X'],
+    ['X', 'X', 'T', 'X', 'X']
+]
+
+spaces = []
+mapList = []
+
+def searchTeacher():
+    for (i, row) in enumerate(world_map):
+        for (j, column) in enumerate(row):
+            if column == 'T':
+                print('선생님 위치확인')
+                print(i, j)
+                searchStudent(i, j)
+
+            if column == 'X':
+                print('빈공간 확인')
+                spaces.append([i, j])
+
+def searchStudent(tRow, tColumn):
+    for (i, el) in enumerate(world_map[tRow]):
+        if el == 'X':
+            print('학생 검거')
+        if el == 'O':
+            print('장애물')
+
+    for (i, el) in enumerate(world_map):
+        if el[tColumn] == 'X':
+            print('학생 검거')
+        if el[tColumn] == 'O':
+            print('장애물')
+
+def makeObstacle():
+    for data in combinations(spaces, 1):
+        for x, y in data:
+            world_map[x][y] = 'O'
+
+searchTeacher()
+makeObstacle()
+
