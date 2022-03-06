67ee2ce0757e4d480b2d15b06f0f2f44deee718c
JinHeok Kim <bustreedev@gmail.com>
2022-03-06 15:07:19 +0900

    국영수 문제풀이

---
+++ jobcodingtest/프로그래머스 문제풀이/23_국영수.py
@@ -0,0 +1,30 @@
+# 국영수
+
+# n = int(input())
+# students = []
+#
+# for _ in range(n):
+#     students.append(input().split())
+
+
+n = 12
+students = [
+    ['Junkyu', '50', '60', '100'],
+    ['Sangkeun', '80', '60', '50'],
+    ['Sunyoung', '80', '70', '100'],
+    ['Soong', '50', '60', '90'],
+    ['Haebin', '50', '60', '100'],
+    ['Kangsoo', '60', '80', '100'],
+    ['Donghyuk', '80', '60', '100'],
+    ['Sei', '70', '70', '70'],
+    ['Wonseob', '70', '70', '90'],
+    ['Sanghyun', '70', '70', '80'],
+    ['nsj', '80', '80', '80'],
+    ['Taewhan', '50', '60', '90']
+]
+
+result = sorted(students, key = lambda x : (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+print(result)
+for i in result:
+    print(i[0])
